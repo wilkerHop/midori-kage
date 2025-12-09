bd15f878049dd8814ff372759b6c9ac4c32bea84
Wilker Ribeiro <rekliw97@gmail.com>
2025-12-08 23:58:43 -0300

    chore(ci): Setup JS environment and GitHub Actions

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+const globals = require("globals");
+const js = require("@eslint/js");
+const prettier = require("eslint-plugin-prettier/recommended");
+
+module.exports = [
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.webextensions,
+        chrome: "readonly",
+        SELECTORS: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": ["warn", { "varsIgnorePattern": "^_" }],
+      "no-console": "off"
+    }
+  }
+];
